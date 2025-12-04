bd2cdffbca473ac63c65a31dccbdc925cfb947c5
Justin Davis Acero <ovrg215@gmail.com>
2025-12-04 20:41:56 -0300

    Forzar python3.11 en build.sh para compatibilidad de dependencias

--- build.sh
+++ build.sh
@@ -2,7 +2,11 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
-    
+# 1. Instalar dependencias usando python3.11 (Soluciona el error de Pillow)
+python3.11 -m pip install -r requirements.txt
+
+# 2. Recolectar archivos estáticos
+python3.11 manage.py collectstatic --no-input
+
+# 3. Aplicar migraciones
+python3.11 manage.py migrate
