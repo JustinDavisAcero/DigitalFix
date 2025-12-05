e7a2e2fe741daeab93d5c7155e3878a11417e449
Justin Davis Acero <ovrg215@gmail.com>
2025-12-04 22:38:02 -0300

    Fix Python version for Render

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install --upgrade pip
 pip install -r requirements.txt
-
-python manage.py collectstatic --noinput
+python manage.py collectstatic --no-input
 python manage.py migrate
+
 
