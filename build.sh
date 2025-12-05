4b71428542101308a2558cadd4b67c6e45daa81f
Justin Davis Acero <ovrg215@gmail.com>
2025-12-04 22:20:17 -0300

    Fix build.sh permissions

--- build.sh
+++ build.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Actualizar pip
 python3.11 -m pip install --upgrade pip
-
-# Instalar dependencias en python3.11
 python3.11 -m pip install -r requirements.txt
-
-# Instalar gunicorn en python3.11 (IMPORTANTE)
 python3.11 -m pip install gunicorn
 
-# Collect static
 python3.11 manage.py collectstatic --noinput
-
-# Migraciones
 python3.11 manage.py migrate
