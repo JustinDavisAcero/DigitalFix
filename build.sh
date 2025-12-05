03dcbdaa5a6ef75beb18c0d123dcba3e8d5b945e
Justin Davis Acero <ovrg215@gmail.com>
2025-12-04 22:15:19 -0300

    Forzar python3.11 en build.sh para compatibilidad de dependencias

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Asegurar pip actualizado
+# Actualizar pip
 python3.11 -m pip install --upgrade pip
 
-# Instalar dependencias EN python3.11
+# Instalar dependencias en python3.11
 python3.11 -m pip install -r requirements.txt
 
-# FORZAR la instalación de gunicorn dentro de Python3.11
+# Instalar gunicorn en python3.11 (IMPORTANTE)
 python3.11 -m pip install gunicorn
 
-# Recolectar archivos estáticos
-python3.11 manage.py collectstatic --no-input
+# Collect static
+python3.11 manage.py collectstatic --noinput
 
-# Aplicar migraciones
+# Migraciones
 python3.11 manage.py migrate
