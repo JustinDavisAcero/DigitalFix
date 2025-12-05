87dbcc7ffc1272bcb94a2cce3d29fe93ebe9f371
Justin Davis Acero <ovrg215@gmail.com>
2025-12-04 22:34:59 -0300

    Corregir Procfile para Render

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python3.11 -m gunicorn DigitalFixProject.wsgi:application
+web: gunicorn DigitalFixProject.wsgi:application
--- build.sh
+++ build.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Actualizar pip
-python3.11 -m pip install --upgrade pip --break-system-packages
+pip install --upgrade pip
+pip install -r requirements.txt
 
-# Instalar dependencias
-python3.11 -m pip install -r requirements.txt --break-system-packages
+python manage.py collectstatic --noinput
+python manage.py migrate
 
-# Asegurar instalación de gunicorn
-python3.11 -m pip install gunicorn --break-system-packages
-
-# Recolectar estáticos
-python3.11 manage.py collectstatic --noinput
-
-# Migraciones
-python3.11 manage.py migrate
