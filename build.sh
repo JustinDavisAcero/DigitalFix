dbfa2ab35e6d066cd71b31f78fad4ddc44f81d0d
Justin Davis Acero <ovrg215@gmail.com>
2025-12-04 22:22:45 -0300

    Fix: build.sh permissions and pip flags

--- build.sh
+++ build.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 set -o errexit
 
-python3.11 -m pip install --upgrade pip
-python3.11 -m pip install -r requirements.txt
-python3.11 -m pip install gunicorn
+# Actualizar pip
+python3.11 -m pip install --upgrade pip --break-system-packages
 
+# Instalar dependencias
+python3.11 -m pip install -r requirements.txt --break-system-packages
+
+# Asegurar instalación de gunicorn
+python3.11 -m pip install gunicorn --break-system-packages
+
+# Recolectar estáticos
 python3.11 manage.py collectstatic --noinput
+
+# Migraciones
 python3.11 manage.py migrate
